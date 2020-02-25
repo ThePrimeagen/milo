08080fb05f1e03623cb4ad3f6118b20a10621fe6
Mattia Toso <mtoso@netflix.com>
2020-02-25 12:02:34 -0800

    add rollup for node

---
+++ rollup-target-plugin.js
@@ -0,0 +1,13 @@
+import path from 'path';
+
+export default function target({ target }) {
+    return {
+        name: 'target',
+        resolveId (source, importer) {
+            if (source.includes('#{target}')) {
+                const targetPath = source.replace('#{target}', target) + '.js';
+                return path.resolve(path.dirname(importer), targetPath);
+            }        
+        },
+    }
+}
