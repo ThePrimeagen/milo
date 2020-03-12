ef472fd48bf9f4e9d9866e5e0572a6364f624947
Anders Bakken <agbakken@gmail.com>
2020-03-12 14:48:26 -0700

    Add lint-wrapper script to make it produce output that's more palatable for the best editor in the history of the cosmos.

---
+++ bin/lint.js
@@ -0,0 +1,36 @@
+const child_process = require("child_process");
+const path = require("path");
+const fs = require("fs");
+
+child_process.execFile(path.join(__dirname, "../node_modules/.bin/tslint"),
+                       [ "--project", path.join(__dirname, "..") ],
+                       (error, stdout, stderr) => {
+                           // if (error) {
+                           //     console.error("Failed to run tslint", error);
+                           // }
+                           if (stdout) {
+                               let file;
+                               console.log(stdout.split("\n").map(line => {
+                                   const match = /^(\/.*):[0-9]+:[0-9]+$/.exec(line);
+                                   if (match) {
+                                       file = match[1];
+                                       return undefined;
+                                   }
+
+                                   if (!file) {
+                                       return undefined;
+                                   }
+
+                                   const error = /^ERROR: ([0-9]+):([0-9]+) *(.*)$/.exec(line);
+                                   if (!error) {
+                                       return undefined;
+                                   }
+                                   return `${file}:${error[1]}:${error[2]}: error: ${error[3]}`;
+                               }).filter(x => x).join("\n"));
+                               // console.log("got stdout", typeof stdout);
+                           }
+                           if (stderr) {
+                               console.error(stderr);
+                           }
+                           process.exit(error ? 1 : 0);
+                       });
