bd81298eee664a91974bf97d29d3de41bd5ebe39
Anders Bakken <agbakken@gmail.com>
2020-03-18 15:50:48 -0700

    Try to use rollup-plugin-babel

--- rollup.nrdp.js
+++ rollup.nrdp.js
@@ -2,19 +2,27 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import target from "./rollup-target-plugin";
+import babel from "rollup-plugin-babel";
+import pkg from "./package.json";
 
 export default {
-    input: 'build/nrdp/milo.js',
+    input: "build/nrdp/milo.js",
     output: {
-        dir: 'dist/',
-        format: 'iife',
+        dir: "dist/",
+        format: "esm",
+        file: pkg.module,
         name: "milo",
         exports: "named"
     }, plugins: [
         target({
-            target: 'nrdp'
+            target: "nrdp"
         }),
         resolve(),
-        commonjs()
+        commonjs(),
+        babel({
+            exclude: "node_modules/**",
+            include: "node_modules/emittery/**",
+            babelrc: false
+        })
     ]
 };
