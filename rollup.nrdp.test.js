ef2e4658f5e42271e6f77df911e59d481f8f831c
Anders Bakken <agbakken@gmail.com>
2020-04-07 22:14:44 -0700

    semicolon

--- rollup.nrdp.test.js
+++ rollup.nrdp.test.js
@@ -1,5 +1,5 @@
 import config from './rollup.nrdp.js';
-config.input = "build/nrdp/autobahn/runner/nrdp/entry.js"
+config.input = "build/nrdp/autobahn/runner/nrdp/entry.js";
 config.output.name = 'entry';
 config.output.file = 'dist/nrdp.autobahn.js';
 
