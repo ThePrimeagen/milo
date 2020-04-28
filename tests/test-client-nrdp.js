986813228f471aa23a4d8a6d22aaddd4efc180b2
Anders Bakken <agbakken@gmail.com>
2020-04-27 17:13:11 -0700

    WIP: git-sync

--- tests/test-client-nrdp.js
+++ tests/test-client-nrdp.js
@@ -4,67 +4,99 @@ var idx = root.lastIndexOf("test-client-nrdp.js");
 var testClient = root.substr(0, idx) + "test-client.js";
 nrdp.gibbon.loadScript({url: testClient, async: false});
 
-nrdp.l("loading", test, typeof runTests);
-
+nrdp.l(nrdp.js_options);
 var http = nrdp.js_options.MILO_TEST_HTTP_SERVER || "http://localhost:60000";
 var https = nrdp.js_options.MILO_TEST_HTTPS_SERVER || "https://milo.netflix.com:60001";
+if (http === "0")
+    http = undefined;
+if (https === "0")
+    https = undefined;
+
+var testNrdp = nrdp.js_options.MILO_TEST_NRDP !== "0";
+var testMilo = nrdp.js_options.MILO_TEST_MILO !== "0";
 
 function fixReq(req, milo) {
     req.milo = milo;
     req.ipAddresses = [ "127.0.0.1" ];
+    req.cache = "no-cache";
     req.dnsType = 5;
     req.dnsName = "milo.netflix.com";
     return req;
 }
 
-function test()
-{
-    nrdp.gibbon.load({url: "http://localhost:60000/?size=16384&gzip", milo: true}, function(response) {
-        nrdp.l("got response", response.data.length);
-    });
-}
-test();
-
 var names = [];
 var times = [];
 
-if (false) {
 Promise.resolve().
     then(function() {
+        if (!testNrdp || !https)
+            return undefined;
         names.push("Resource manager https");
         times.push(nrdp.mono());
-        return undefined;
-        return runTests(https, function(req, cb) {
-            nrdp.gibbon.load(fixReq(req, false), cb);
+        // return undefined;
+        return runTests({
+            name: names[names.length - 1],
+            server: https,
+            load: function(req, cb) {
+                nrdp.gibbon.load(fixReq(req, false), cb);
+            },
+            log: nrdp.l.bind(nrdp)
         });
     }).
     then(function() {
+        if (!testNrdp || !http)
+            return undefined;
+
         names.push("Resource manager http");
         var now = nrdp.mono();
-        times[times.length - 1] = now - times[times.length - 1];
+        if (times.length)
+            times[times.length - 1] = now - times[times.length - 1];
         times.push(now);
-        return undefined;
-        return runTests(http, function(req, cb) {
-            nrdp.gibbon.load(fixReq(req, false), cb);
+        // return undefined;
+        return runTests({
+            name: names[names.length - 1],
+            server: http,
+            load: function(req, cb) {
+                nrdp.gibbon.load(fixReq(req, false), cb);
+            },
+            log: nrdp.l.bind(nrdp)
         });
     }).
     then(function() {
+        if (!testMilo || !https)
+            return undefined;
+
         names.push("Milo https");
         var now = nrdp.mono();
-        times[times.length - 1] = now - times[times.length - 1];
+        if (times.length)
+            times[times.length - 1] = now - times[times.length - 1];
         times.push(now);
-        return undefined;
-        return runTests(https, function(req, cb) {
-            nrdp.gibbon.load(fixReq(req, true), cb);
+        // return undefined;
+        return runTests({
+            name: names[names.length - 1],
+            server: https,
+            load: function(req, cb) {
+                nrdp.gibbon.load(fixReq(req, true), cb);
+            },
+            log: nrdp.l.bind(nrdp)
         });
     }).
     then(function() {
+        if (!testMilo || !http)
+            return undefined;
+
         names.push("Milo http");
         var now = nrdp.mono();
-        times[times.length - 1] = now - times[times.length - 1];
+        if (times.length)
+            times[times.length - 1] = now - times[times.length - 1];
         times.push(now);
-        return runTests(http, function(req, cb) {
-            nrdp.gibbon.load(fixReq(req, true), cb);
+        return runTests({
+            name: names[names.length - 1],
+            server: http,
+            load: function(req, cb) {
+                nrdp.gibbon.load(fixReq(req, true), cb);
+            },
+            log: nrdp.l.bind(nrdp)
         });
     }).
     then(function() {
@@ -84,4 +116,3 @@ Promise.resolve().
 
         nrdp.exit(1);
     });
-}
--- tests/test-client.js
+++ tests/test-client.js
@@ -1,4 +1,4 @@
-function runTests(server, loadFunc)
+function runTests(options)
 {
     var table = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
 
@@ -14,10 +14,12 @@ function runTests(server, loadFunc)
     }
     var chunked = [ false, true ];
     var compression = [ undefined, "gzip", "deflate" ];
+    var testCount = 0;
+    var testIdx = 0;
 
     function createReq(payload, chunked, compression) {
         return new Promise(function(resolve, reject) {
-            var url = server;
+            var url = options.server;
             url += "/?size=" + payload.length;
             if (chunked)
                 url += "&chunked";
@@ -27,7 +29,7 @@ function runTests(server, loadFunc)
             var id = setTimeout(function() {
                 reject(new Error("request " + url + " timed out"));
             }, 30000);
-            loadFunc({url: url}, function(response) {
+            options.load({url: url}, function(response) {
                 clearTimeout(id);
                 if (response.statusCode !== 200) {
                     nrdp.l.error(response);
@@ -42,6 +44,9 @@ function runTests(server, loadFunc)
                     reject(new Error("Bad payload expected " + payload.length + " bytes, but got " + len + " for " + url));
                     return;
                 }
+                if (++testIdx % 10 === 0 || testIdx === testCount) {
+                    options.log(options.name, testIdx + "/" + testCount);
+                }
                 resolve();
             });
         });
@@ -51,6 +56,7 @@ function runTests(server, loadFunc)
     payloads.forEach(function payloadFunc(payload) {
         chunked.forEach(function chunkedFunc(chunky) {
             compression.forEach(function compressionFunc(compressionType) {
+                ++testCount;
                 promise = promise.then(createReq.bind(undefined, payload, chunky, compressionType));
             });
         });
